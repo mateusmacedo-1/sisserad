8d3cb373d83f1a528c0ceb76d10944cf196eb0f2
mateus macedo <mateuswesley314@gmail.com>
2024-06-02 10:52:49 -0300

    testes model cliente

--- sisserad/clientes/models.py
+++ sisserad/clientes/models.py
@@ -4,14 +4,14 @@ from core.models import BaseModel
 class Cliente(BaseModel):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
     nome = models.CharField(max_length=255, blank=False, null=False)
-    documento = models.CharField(max_length=50, blank=False, null=False)
-    email = models.EmailField(max_length=50, blank=False, null=False)
+    documento = models.CharField(max_length=50, blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=50, blank=False, null=False, unique=True)
     telefone_fixo = models.CharField(max_length=20, blank=True, null=True)
     celular = models.CharField(max_length=20)
     ativo = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'{self.nome.capitalize()}({self.documento})'
+        return f'{self.nome.capitalize()}(Documento: {self.documento})'
 
 class Equipamento(BaseModel):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
--- sisserad/clientes/tests.py
+++ sisserad/clientes/tests.py
@@ -1,3 +1,16 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
+from clientes.models import Cliente
+
 # Create your tests here.
+class ClienteModelTest(TestCase):
+    def test_somente_um_cliente_por_documento(self):
+        Cliente.objects.create(nome="João", documento="12345678901", email="joao@gmail.com", celular="999999999")
+        with self.assertRaises(IntegrityError):
+            Cliente.objects.create(nome="Maria", documento="12345678901", email="jose@gmail.com", celular="999999999")
+    def test_somente_um_cliente_por_email(self):
+        Cliente.objects.create(nome="João", documento="12345678901", email="joao@gmail.com", celular="999999999")
+        with self.assertRaises(IntegrityError):
+            Cliente.objects.create(nome="Maria", documento="123555221", email="joao@gmail.com", celular="999999999")
+        
