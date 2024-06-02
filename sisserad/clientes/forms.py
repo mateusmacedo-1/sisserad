a6800574cc94d6dbf6ac0e518c88f27d2c9aafab
mateus macedo <mateuswesley314@gmail.com>
2024-06-02 10:00:52 -0300

    resolvendo problema de passar cliente pro form

--- sisserad/clientes/forms.py
+++ sisserad/clientes/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from clientes.models import Cliente, Equipamento
+from clientes.models import Cliente, Endereco, Equipamento
 
 
 class EquipamentoForm(forms.ModelForm):
@@ -8,8 +8,32 @@ class EquipamentoForm(forms.ModelForm):
         model = Equipamento
         fields = ["nome", "fabricante", "modelo", "numero_serie", "ano_fabricacao", "parametros_variaveis", "endereco"]
     def __init__(self, *args, **kwargs):
-        self.cliente_id = kwargs.pop('cliente_id', None)
+        cliente_id = kwargs.pop('cliente_id', None)
+        self.cliente = Cliente.objects.get(pk=cliente_id)
         super(EquipamentoForm, self).__init__(*args, **kwargs)
-        if self.cliente_id:
-            cliente = Cliente.objects.get(pk=self.cliente_id)
-            self.fields['endereco'].queryset = cliente.endereco_set.all()
+        if self.cliente:
+            self.fields['endereco'].queryset = self.cliente.endereco_set.all()
+    def save(self, commit=True):
+        equipamento = super(EquipamentoForm, self).save(commit=False)
+        if self.cliente:
+            equipamento.cliente = self.cliente
+        if commit:
+            equipamento.save()
+        return equipamento    
+        
+
+class EnderecoForm(forms.ModelForm):
+    class Meta:
+        model = Endereco
+        fields = ['logradouro', 'numero', 'cep', 'cidade', 'estado']
+    def __init__(self, *args, **kwargs):
+        cliente_id = kwargs.pop('cliente_id', None)
+        self.cliente = Cliente.objects.get(pk=cliente_id)
+        super(EnderecoForm, self).__init__(*args, **kwargs)
+    def save(self, commit=True):
+        endereco = super(EnderecoForm, self).save(commit=False)
+        if self.cliente:
+            endereco.cliente = self.cliente
+        if commit:
+            endereco.save()
+        return endereco 
--- sisserad/clientes/urls.py
+++ sisserad/clientes/urls.py
@@ -6,6 +6,7 @@ app_name = "clientes"
 urlpatterns = [
     path("/<int:cliente_id>/", views.cliente_detail, name="detail"),
     path("/<int:cliente_id>/equipamento/new", views.cliente_equipamento_form, name="equipamento-form"),
+    path("/<int:cliente_id>/endereco/new", views.cliente_endereco_form, name="endereco-form"),
     path("", views.cliente_list, name="list"),
 
 ]
