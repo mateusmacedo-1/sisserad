fc0fe1f40c847d829c1112361b0e45c8993c4b89
mateus macedo <mateuswesley314@gmail.com>
2024-06-03 09:53:12 -0300

    validação e teste de endereço único para o cliente

--- sisserad/clientes/models.py
+++ sisserad/clientes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from core.models import BaseModel
-
+from django.core.exceptions import (
+    ValidationError,
+)
 class Cliente(BaseModel):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
     nome = models.CharField(max_length=255, blank=False, null=False)
@@ -20,11 +22,20 @@ class Equipamento(BaseModel):
     modelo = models.CharField(max_length=50)
     numero_serie = models.CharField(max_length=50)
     ano_fabricacao = models.IntegerField()
-    parametros_variaveis = models.CharField(max_length=255)
+    parametros_variaveis = models.CharField(max_length=255, null=True, blank=True)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     ativo = models.BooleanField(default=True)
     endereco = models.ForeignKey('Endereco', on_delete=models.SET_NULL, blank=True, null=True)
-
+    
+    def clean(self, *args, **kwargs):
+        if self.cliente.equipamento_set.filter(numero_serie=self.numero_serie).exists():
+            raise ValidationError('Equipamento já cadastrado para este cliente')
+        super().clean(*args, **kwargs)
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+        
     def __str__(self):
         return f'{self.nome} Nº Série: {self.numero_serie}'
 
@@ -32,12 +43,21 @@ class Endereco(BaseModel):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
     logradouro = models.CharField(max_length=255)
     numero = models.CharField(max_length=10)
-    complemento = models.CharField(max_length=255)
+    complemento = models.CharField(max_length=255, null=True, blank=True)
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
     cep = models.CharField(max_length=10)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     ativo = models.BooleanField(default=True)
+
+    def clean(self, *args, **kwargs):
+        if self.cliente.endereco_set.filter(cep=self.cep, numero=self.numero).exists():
+            raise ValidationError('Endereço já cadastrado para este cliente')
+        super().clean(*args, **kwargs)
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.logradouro}, {self.numero} CEP:{self.cep}'
--- sisserad/clientes/tests.py
+++ sisserad/clientes/tests.py
@@ -3,6 +3,11 @@ from django.test import TestCase
 
 from clientes.models import Cliente
 
+from django.core.exceptions import (
+    ValidationError,
+)
+
+
 # Create your tests here.
 class ClienteModelTest(TestCase):
     def test_somente_um_cliente_por_documento(self):
@@ -13,4 +18,13 @@ class ClienteModelTest(TestCase):
         Cliente.objects.create(nome="João", documento="12345678901", email="joao@gmail.com", celular="999999999")
         with self.assertRaises(IntegrityError):
             Cliente.objects.create(nome="Maria", documento="123555221", email="joao@gmail.com", celular="999999999")
+class EnderecoModelTest(TestCase):
+    """
+        A unicidade do endereço é testada com uma combinação de cep e número.
+    """
+    def test_endereco_deve_ser_unico_por_cliente(self):
+        c = Cliente.objects.create(nome="Maria", documento="123555221", email="joao@gmail.com", celular="999999999")
+        c.endereco_set.create(logradouro="Rua 1", numero="123", cep="12345678", cidade="mossoro", estado="rn")
+        with self.assertRaises(ValidationError):
+            c.endereco_set.create(logradouro="Rua 2", numero="123", cep="12345678", cidade="mossoro", estado="rn")
         
