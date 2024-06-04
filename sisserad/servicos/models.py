8a95c062b2511089735ec2d9ded30551197fbb8c
mateus macedo <mateuswesley314@gmail.com>
2024-06-04 17:00:07 -0300

    criando app de serviços

---
+++ sisserad/servicos/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from sisserad.core.models import BaseModel
+STATUS_CHOICES = [
+        ('AB', 'Aberto'),
+        ('FI', 'Finalizado'),
+        ('RE', 'Revisado'),
+        ('PU', 'Publicado'),
+]
+class Servico(BaseModel):
+    cliente = models.ForeignKey('clientes.Cliente', on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AB')
+    
+    def __str__(self):
+        return f'{self.cliente.nome}'
+class TipoAtividade(BaseModel):
+    nome = models.CharField(max_length=50)
+    link_form_base = models.URLField()
+    
+    def __str__(self):
+        return self.nome
+    
+class Atividade(BaseModel):
+    tipo_atividade = models.ForeignKey(TipoAtividade, on_delete=models.DO_NOTHING)
+    equipamento = models.ForeignKey('clientes.Equipamento', on_delete=models.DO_NOTHING)
+    previsao_finalizacao = models.DateTimeField()
+    vencimento = models.DateField()
+    responsavel = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data_solicitação = models.DateTimeField()
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AB')
+    link_formulario = models.URLField()
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.tipo_atividade.nome} - {self.equipamento.nome}'
+    
+
+
+    
