4c70b78b483e4c189dc21dcdf922c6abe855a846
mateus macedo <mateuswesley314@gmail.com>
2024-06-04 16:59:15 -0300

    add created_by ao basemodel para
    poder registrar quem criou os registros

--- sisserad/core/models.py
+++ sisserad/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from django.contrib import admin
 
 # Create your models here.
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     ativo = models.BooleanField(default=True)
+    created_by = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
     class Meta:
         abstract = True
