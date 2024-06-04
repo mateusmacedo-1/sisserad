be03b5f62f1654a1eabdc89aaa4a5c06593615aa
mateus macedo <mateuswesley314@gmail.com>
2024-06-04 09:55:59 -0300

    colocando ativo para a baseentity

--- sisserad/clientes/models.py
+++ sisserad/clientes/models.py
@@ -8,7 +8,7 @@ class Cliente(BaseModel):
     email = models.EmailField(max_length=50, blank=False, null=False, unique=True)
     telefone_fixo = models.CharField(max_length=20, blank=True, null=True)
     celular = models.CharField(max_length=20)
-    ativo = models.BooleanField(default=True)
+    
 
     def __str__(self):
         return f'{self.nome.capitalize()}(Documento: {self.documento})'
@@ -22,7 +22,6 @@ class Equipamento(BaseModel):
     ano_fabricacao = models.IntegerField()
     parametros_variaveis = models.CharField(max_length=255, null=True, blank=True)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
-    ativo = models.BooleanField(default=True)
     endereco = models.ForeignKey('Endereco', on_delete=models.SET_NULL, blank=True, null=True)
         
     def __str__(self):
@@ -36,9 +35,7 @@ class Endereco(BaseModel):
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
     cep = models.CharField(max_length=10)
-    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
-    ativo = models.BooleanField(default=True)
-        
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)  
         
     def __str__(self):
         return f'{self.logradouro}, {self.numero} CEP:{self.cep}'
--- sisserad/core/models.py
+++ sisserad/core/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    ativo = models.BooleanField(default=True)
     class Meta:
         abstract = True
